398aa8f3b814ba8aa2ab29e12e21f4a84685b29f
MissMoneyBags <106129216+DanielaDeMoura@users.noreply.github.com>
2023-12-10 00:06:00 +0000

    added code to logic and html files.

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,3 +1,20 @@
+
+document.addEventListener("DOMContentLoaded", function () {
+  var startButton = document.getElementById("start");
+  startButton.addEventListener("click", function () {
+    startQuiz();
+  });
+});
+
+
+
+// retrieve the first question
+function getFirstQuestion() {
+  return questions[0];
+}
+
+
+
 const correctAudio = new Audio("./assets/sfx/correct.wav");
 const incorrectAudio = new Audio("./assets/sfx/incorrect.wav");
 
